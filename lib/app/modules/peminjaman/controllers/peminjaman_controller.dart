096d752c71999b5b7c8ca9cd58f91badfb1bbbc7
fushigo <rizky23ska@gmail.com>
2024-01-18 15:34:10 +0700

    peminjam

--- lib/app/modules/peminjaman/controllers/peminjaman_controller.dart
+++ lib/app/modules/peminjaman/controllers/peminjaman_controller.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:peminjam_perpustakaan_app/app/data/constant/endpoint.dart';
 import 'package:peminjam_perpustakaan_app/app/data/model/response_pinjam.dart';
 import 'package:peminjam_perpustakaan_app/app/data/provider/api_provider.dart';
+import 'package:peminjam_perpustakaan_app/app/data/provider/storage_provider.dart';
 
 class PeminjamanController extends GetxController with StateMixin<List<DataPinjam>>{
   //TODO: Implement PeminjamanController
@@ -28,8 +29,8 @@ class PeminjamanController extends GetxController with StateMixin<List<DataPinja
   Future<void> getData() async {
     change(null, status: RxStatus.loading());
     try {
-      final response = await ApiProvider.instance().get(Endpoint
-          .pinjam);
+      final response = await ApiProvider.instance().get("${Endpoint
+          .pinjam}/${StorageProvider.read(StorageKey.idUser)}");
       if (response.statusCode == 200) {
         final ResponsePinjam responsePinjam = ResponsePinjam.fromJson(response.data);
         if (responsePinjam.data!.isEmpty) {
